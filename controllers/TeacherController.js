36d461487552ed3863e833777980864a6db12939
ShavkatjonT <105767759+ShavkatjonT@users.noreply.github.com>
2023-12-08 14:39:05 +0500

    update

--- controllers/TeacherController.js
+++ controllers/TeacherController.js
@@ -542,6 +542,13 @@ class TeachersController {
                 }
             });
 
+            const teacherGroups = await TeacherGroups.findAll({
+                where: {
+                    status: 'active',
+                    teacher_id: id
+                }
+            });
+
 
             const statistics = await TeacherStatistics.findAll({
                 where: {
@@ -559,10 +566,10 @@ class TeachersController {
                 ],
             });
 
-
             const result = groups.map((group) => {
                 const groupStatistics = statistics.filter((stat) => stat.group_id === group.id);
                 const latestStatistics = [];
+                const teacher_groups_one = teacherGroups.find((el) => el.group_id == group.id);
                 const seenMonths = new Set();
                 for (const stat of groupStatistics) {
                     const monthKey = stat.createdAt.toISOString().slice(0, 7);
@@ -574,15 +581,15 @@ class TeachersController {
                         });
                     }
                 }
-                return {
-                    id: group.id,
-                    name: group.name,
-                    interest: group.sale,
-                    statistics: latestStatistics,
-                };
-            });
-
-
+                if (teacher_groups_one) {
+                    return {
+                        id: group.id,
+                        name: group.name,
+                        interest: group.sale,
+                        statistics: latestStatistics,
+                    };
+                }
+            }).filter((el) => el && el);
             return res.json(result)
         } catch (error) {
             console.log(453, error.stack);
